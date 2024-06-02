99348423bf3ea4f779da6c588672704abccd3b73
TalFlint1 <tal.flint@live.biu.ac.il>
2024-06-02 13:27:36 +0300

    added errors message to pass fields

--- registration-page/src/registerPage/Register.js
+++ registration-page/src/registerPage/Register.js
@@ -3,7 +3,6 @@ import { ReactComponent as ErrorSign } from '../assets/exclamation_point.svg';
 import './Register.css';
 import youtubeLogo from '../assets/youtubelogo.png';
 
-
 const Register = () => {
   const [formData, setFormData] = useState({
     username: '',
@@ -15,30 +14,45 @@ const Register = () => {
 
   const [passwordError, setPasswordError] = useState('');
   const [passwordValidated, setPasswordValidated] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
 
   const validatePassword = (password) => {
     // Updated regular expression to include special characters !@#$%^&*
-    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,16}$/;
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d!@#$%^&*]{8,16}$/;
     if (!passwordRegex.test(password)) {
-      return 'Password must be between 8-16 characters and combine letters and numbers.'
+      return 'Password must be between 8-16 characters and combine letters and numbers.';
     }
     return null; // Password is valid
   };
 
+  const validateConfirmPassword = (confirmPassword) => {
+    if (confirmPassword !== formData.password) {
+      return "The passwords don't match. Try again.";
+    }
+    return null; // Passwords match
+  };
+  
+
   const handleChange = (e) => {
     const { name, value, files } = e.target;
-
+  
     if (name === 'password') {
       const error = validatePassword(value);
       setPasswordError(error);
       setPasswordValidated(true);
     }
-
+  
+    if (name === 'confirmPassword') {
+      const error = validateConfirmPassword(value);
+      setConfirmPasswordError(error);
+    }
+  
     setFormData({
       ...formData,
       [name]: files ? files[0] : value,
     });
   };
+  
 
   const handlePasswordBlur = () => {
     const error = validatePassword(formData.password);
@@ -48,6 +62,11 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Check if passwords match
+    if (formData.password !== formData.confirmPassword) {
+      setConfirmPasswordError("The passwords don't match. Try again.");
+      return;
+    }
     // Handle form submission
     console.log(formData);
   };
@@ -55,7 +74,7 @@ const Register = () => {
   return (
     <div className="register-container">
       <div className="logo-container">
-      <img src={youtubeLogo} alt="YouTube Logo" className="youtube-logo" />
+        <img src={youtubeLogo} alt="YouTube Logo" className="youtube-logo" />
       </div>
       <form className="register-form" onSubmit={handleSubmit}>
         <h2>Sign up</h2>
@@ -78,8 +97,12 @@ const Register = () => {
         />
         {passwordValidated && passwordError && (
           <div className="error-message">
-            <ErrorSign className="error-icon" />
-            {passwordError}
+            <div className="error-icon-container">
+              <ErrorSign className="error-icon" />
+            </div>
+            <div className="pass-error-text-container">
+              {passwordError}
+            </div>
           </div>
         )}
         <label>Confirm Password:</label>
@@ -90,6 +113,16 @@ const Register = () => {
           onChange={handleChange}
           required
         />
+        {confirmPasswordError && (
+          <div className="error-message">
+            <div className="error-icon-container">
+              <ErrorSign className="error-icon" />
+            </div>
+            <div className="conf-pass-error-text-container">
+              {confirmPasswordError}
+            </div>
+          </div>
+        )}
         <label>Name:</label>
         <input
           type="text"
