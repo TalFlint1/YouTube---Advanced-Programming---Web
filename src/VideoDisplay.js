b0507768541b838e6c24c2e67ac3a5c62453ff71
shiradeu <96350949+shiradeu@users.noreply.github.com>
2024-06-05 21:43:56 +0300

    add metadata of video- in json+UI

--- src/VideoDisplay.js
+++ src/VideoDisplay.js
@@ -1,20 +1,62 @@
-
-
 import React from 'react';
 
-const VideoDisplay = ({ title, description, videoUrl, thumbnailUrl, duration ,isDarkMode }) => {
+const VideoDisplay = ({ title, description, videoUrl, thumbnailUrl, duration,owner ,isDarkMode ,views,time_publish ,time_type}) => {
+  const videoDisplayStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: isDarkMode ? '#black' : '#black',
+    color: isDarkMode ? '#black' : '#black',
+    padding: '20px',
+    borderRadius: '10px',
+    marginBottom: '20px',
+  };
+
+  const videoContentStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: '600px',
+    width: '100%',
+  };
+
+  const videoPlayerStyle = {
+    maxWidth: '100%',
+  };
+
+  const videoInfoStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    fontSize: '14px',
+  };
+
   return (
-    <div  className={`video-display ${isDarkMode ? 'dark-mode' : ''}`}>
-      <div className="video-thumbnail">
-        <img src={thumbnailUrl} alt={title} />
-        <span className="video-duration">{duration}</span>
-      </div>
-      <video controls>
-        <source src={videoUrl} type="video/mp4" />
-      </video>
-      <div className="video-info">
-        <h2>{title}</h2>
-        <p>{description}</p>
+    <div style={videoDisplayStyle}>
+      <div style={videoContentStyle}>
+    
+        <div style={videoPlayerStyle}>
+          <video controls style={videoPlayerStyle}>
+            <source src={videoUrl} type="video/mp4" />
+          </video>
+        </div>
+        <div style={videoInfoStyle}>
+          <div  class="title">
+          <span > {title}</span>
+          </div>
+        <span> <img id="img" draggable="false" class="style-scope yt-img-shadow" alt="" width="28" src="https://yt3.ggpht.com/mdK1Wn2nadJ4WvVbr_BmVtzFJZ4FtYDqfO1L5yCPNokDDn2wnJiHbtDz32CvRoz87OqsICnvVQ=s68-c-k-c0x00ffffff-no-rj"
+        ></img></span>
+        </div>
+        <div style={videoInfoStyle}>
+        <span> </span>
+        <span>{owner}</span>
+        </div>
+        <div style={videoInfoStyle}>
+        <span>Published {time_publish} {time_type} ago</span>
+          <span>
+            {views} views</span>
+        </div>
       </div>
     </div>
   );
