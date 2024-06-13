4fdbd746bcc2f6512ff516d96bb427fb285f0515
shiradeu <96350949+shiradeu@users.noreply.github.com>
2024-06-13 17:07:12 +0300

    change addVideo UI

--- src/components/AddVideoPopup/AddVideoPopup.js
+++ src/components/AddVideoPopup/AddVideoPopup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './AddVideoPopup.css'; // Create this CSS file for styling the popup
-import { faUpload} from '@fortawesome/free-solid-svg-icons';
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const AddVideoPopup = ({ closePopup, addVideo }) => {
@@ -11,7 +11,6 @@ const AddVideoPopup = ({ closePopup, addVideo }) => {
   const time_type = 'hours';
   const time_publish = '0';
   const views = '0';
-  //TODO change this data to the user that connected data
   const owner = ' חמי';
   const user_icon = "https://yt3.ggpht.com/mdK1Wn2nadJ4WvVbr_BmVtzFJZ4FtYDqfO1L5yCPNokDDn2wnJiHbtDz32CvRoz87OqsICnvVQ=s68-c-k-c0x00ffffff-no-rj";
 
@@ -20,7 +19,7 @@ const AddVideoPopup = ({ closePopup, addVideo }) => {
     if (videoFile) {
       console.log('Uploaded video file:', videoFile);
     }
-    addVideo({ title, description, videoUrl, videoFile ,views,time_type,time_publish,owner,user_icon});
+    addVideo({ title, description, videoUrl, videoFile, views, time_type, time_publish, owner, user_icon });
     closePopup();
   };
 
@@ -33,21 +32,28 @@ const AddVideoPopup = ({ closePopup, addVideo }) => {
       <div className="popup-content">
         <button className="close-button" onClick={closePopup}>×</button>
         <h2>Add a New Video</h2>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Title:
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
-          </label>
+        <form onSubmit={handleSubmit} className="add-video-form">
+          <label>Title:</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          
+          <label>Video URL:</label>
+          <input
+            type="url"
+            value={videoUrl}
+            onChange={(e) => setVideoUrl(e.target.value)}
+            required
+          />
           
           <label>
-            Video URL:
-            <input type="url" value={videoUrl} onChange={(e) => setVideoUrl(e.target.value)} required />
-          </label>
-          <label>
-          <FontAwesomeIcon icon={faUpload} />
-              Or Upload Video:
+            <FontAwesomeIcon icon={faUpload} /> Or Upload Video:
             <input type="file" accept="video/*" onChange={handleFileChange} />
           </label>
+          
           <button type="submit">Add Video</button>
           <button type="button" onClick={closePopup}>Cancel</button>
         </form>
